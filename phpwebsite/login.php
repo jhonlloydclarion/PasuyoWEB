4b65e423597054dde31a4781dbefeb65596f4290
Jhon lloyd Clarion <jhonlloydclarion@yahoo.com>
2021-06-23 17:45:13 +0800

    Updated
    hindi parin gumagana

--- phpwebsite/login.php
+++ phpwebsite/login.php
@@ -1,20 +1,27 @@
+
+<?php
+   
+    $config = "front-dev-design/css/index.css";
+    $a = "fuck you!";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="PASUYOWEB/front-dev-design/css/index.css">
-    <title>Document</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="<?php echo $config?>">
+        <title>FAK</title>
+    </head>
 
 <body>
 
     <div class="mainbox">
         <div class="loginbox">
             <div class="log-box">
-                <img class="logo" src="/logo.svg" alt="none.jpeg">
+                <img class="logo" src="/img/logo.svg" alt="none.jpeg">
             </div>
             <div class="input-box">
                 <input class="input" type="text" placeholder="Username">
@@ -26,7 +33,7 @@
                 
             </div>
             <div class="error-box">
-                <p style="color: red;">Username and Password is required</p>
+                <p style="color: red;"> <?php echo $a?>Username and Password is required</p>
             </div>
         </div>
     </div>
